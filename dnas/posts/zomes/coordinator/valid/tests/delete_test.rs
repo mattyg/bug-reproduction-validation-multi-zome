549bd1acb733bde328d1ccfca4e8188f2204b79b
Matt Gabrenya <matt@buildyourweb.app>
2023-10-02 16:57:53 -0700

    refactor: implement delete link test in sweettest

--- dnas/posts/zomes/coordinator/valid/tests/delete_test.rs
+++ dnas/posts/zomes/coordinator/valid/tests/delete_test.rs
@@ -4,7 +4,7 @@ use valid_integrity::*;
 
 
 #[tokio::test(flavor = "multi_thread")]
-async fn create_then_delete_post_test() {
+async fn create_then_delete_entry_test() {
     // Use prebuilt dna file
     let dna_path = std::env::current_dir()
         .unwrap()
@@ -24,5 +24,30 @@ async fn create_then_delete_post_test() {
     // Alice deletes the post
     let _delete_action_hash: ActionHash = conductor
         .call(&alice.zome("valid"), "delete_post", record.action_address())
+        .await;
+}
+
+
+#[tokio::test(flavor = "multi_thread")]
+async fn create_then_delete_link_test() {
+    // Use prebuilt dna file
+    let dna_path = std::env::current_dir()
+        .unwrap()
+        .join("../../../workdir/posts.dna");
+    let dna = SweetDnaFile::from_bundle(&dna_path).await.unwrap();
+
+    // Set up conductors
+    let mut conductor: SweetConductor = SweetConductor::from_config(ConductorConfig::default()).await;
+    let app = conductor.setup_app("demo", &[dna]).await.unwrap();
+    let (alice,) = app.into_tuple();
+    
+    // Alice creates a post
+    let link_ah: ActionHash = conductor
+        .call(&alice.zome("valid"), "create_demo_link", ())
+        .await;
+
+    // Alice deletes the post
+    let _delete_action_hash: ActionHash = conductor
+        .call(&alice.zome("valid"), "delete_demo_link", link_ah)
         .await;
 }
